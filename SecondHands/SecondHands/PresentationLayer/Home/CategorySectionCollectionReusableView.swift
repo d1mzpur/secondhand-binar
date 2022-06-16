6f3ce05c7dd48c56a4c6d903c40479e3f3331f40
Adji Firmansyah <adji.firmansyah27@icloud.com>
2022-06-16 13:53:04 +0700

    Add HomeCollectionView and delete SCHomeViewController

---
+++ SecondHands/SecondHands/PresentationLayer/Home/CategorySectionCollectionReusableView.swift
@@ -0,0 +1,24 @@
+//
+//  CategorySectionCollectionReusableView.swift
+//  SecondHands
+//
+//  Created by Adji Firmansyah on 16/06/22.
+//
+
+import UIKit
+
+class CategorySectionCollectionReusableView: UICollectionReusableView {
+    lazy var label = UILabel()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        label.font = SCLabel(frame: .zero, weight: .bold, size: 14).font
+        label.text = "Telusuri Kategori"
+        label.frame = bounds
+        addSubview(label)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}

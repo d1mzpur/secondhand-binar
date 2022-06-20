a0fb200069e59047cc72e66138d7a287f8408933
Yuda <>
2022-06-20 13:19:27 +0700

    no message

--- SecondHands/SecondHands/PresentationLayer/ScreenView/Login/SCLoginViewController.swift
+++ SecondHands/SecondHands/PresentationLayer/ScreenView/Login/SCLoginViewController.swift
@@ -2,7 +2,7 @@
 //  SCLoginViewController.swift
 //  SecondHands
 //
-//  Created by APPLE on 20/06/22.
+//  Created by APPLE on 18/06/22.
 //
 
 import UIKit
@@ -17,6 +17,13 @@ class SCLoginViewController: UIViewController {
 
     }
     
+    lazy var labelMasuk = UILabel()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        labelMasuk.font = SCLabel(frame: CGRect(x: 0, y: 0, width: 83, height: 36), weight: .bold, size: 24).font
+        labelMasuk.text = "Masuk"
+    }
+    
     lazy var formEmail: SCFormItem = SCFormItem(formType: .normal, formName: "E-mail", placeholder: "Contoh: johndee@gmail.com")
     
     lazy var formPassword: SCFormItem = SCFormItem(formType: .password, formName: "Password", placeholder: "Masukkan password")
@@ -26,11 +33,12 @@ class SCLoginViewController: UIViewController {
     
     lazy var formItemStack: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [
+            labelMasuk,
             formEmail,
             formPassword,
-            formNumberPhone,
             loginButton
             ])
+        stackView.setCustomSpacing(24, after: labelMasuk)
         stackView.setCustomSpacing(16, after: formEmail)
         stackView.setCustomSpacing(24, after: formPassword)
         stackView.axis = .vertical
